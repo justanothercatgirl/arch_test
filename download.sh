711bbf849f0d8a45a4065610d087205227a4b36f
Just Another Catgirl <73675324+justanothercatgirl@users.noreply.github.com>
2023-08-10 12:13:34 +0300

    fixed arguments, no unnecessary ffmpeg calls

--- download.sh
+++ download.sh
@@ -1,57 +1,46 @@
 #personal wrapper for yt-dlp
-
+version=2.2                                                                                                     
+echo youtube downloader wrapper, version ${version}
+ 
 #argc check
-if [ $# -lt 2 ] || [ $1 = -h ] || [ $1 = help ] || [ $1
-= --help ];
+if [ $# -lt 2  ] || [ $1 = -h  ] || [ $1 = help ] || [ $
+1 = --help  ];
 then
-        echo "usage: download <mp3/mp4> <URL> [path]"
+        echo "usage: bash download.sh <mp3/mp4> <URL> [path]"
         exit
 fi
-
+ 
 #define format
 if [ $1 = mp3 ]; then
         arg="-f ba -x --audio-format mp3"
-elif [ $2 = mp4 ]; then
+elif [ $1 = mp4 ]; then
         arg="-f mp4"
 else
         echo unknown format
         exit
 fi
-
+ 
 #if path is passed, use it
 if [ $# -eq 3 ]; then
         path="$3"
 else
         path="/storage/emulated/0/term_download_$1"
 fi
-
-#ugh annoying
-#yt-dlp -s $2
-#
-#echo continue? [Yn]
-#read _
-#
-#if [ read = n ] || [ read = N ]; then
-#       exit
-#fi
-
+ 
 #actual download
-yt-dlp -o "${path}/temp/%(title)s.%(ext)s" ${arg} $2
-
-
+yt-dlp -o "${path}/temp/%(title)s.%(ext)s" --progress ${
+arg} "$2"
+ 
+#change metdata
+restore=`pwd`
 cd "${path}/temp"
 for i in *; do
-        if [ $1 = mp4 ]; then
-                newname="${i%.*}.mp4"
-                ffmpeg -i "$i" "${newname}"
-                mv "${newname}" "../${newname}"
-        else
-                mv "$i" "../$i"
-        fi
+        eyeD3 --quiet --release-date `date +%Y-%m-%dT%H:
+%M:%S` "$i"
+        mv "$i" "${path}"
 done
-
-cd ..
-rm -rf temp
-cd
-
-echo DONE!!!
+cd ${restore}
+                                                        #remove temporary directory
+rm -r "${path}/temp"
+ 
+echo Version ${version}: done!
